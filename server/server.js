a86a3204b146fc0ca8914b0cf5407393fe7560f5
Dev Jana <devjana@Devs-MBP-2.domain>
2020-06-26 14:26:17 -0500

    front end is ready

--- server/server.js
+++ server/server.js
@@ -13,3 +13,12 @@ app.listen( port, ()=>{
     console.log( 'server up:',  port );
 }) //end server up
 // routes
+app.get( '/items', ( req, res )=>{
+    console.log( 'in  /items GET' );
+    res.send( inventory );
+}) //end /items GET
+
+app.post( '/items', ( req, res )=>{
+    console.log( 'in /items POST:', req.body );
+    res.send( 'ribbet' );
+}) //end /items POST
