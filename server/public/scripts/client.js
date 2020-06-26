a4f50e25f1e0962529d65e148970a7761546e304
Dev Jana <devjana@Devs-MBP-2.domain>
2020-06-26 14:40:33 -0500

    post working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -17,12 +17,29 @@ function addItem(){
     }).then( function( response ){
         // back from server, update DOM
         console.log( 'back from server:', response );
+        getItems();
     }).catch( function( err ){
         alert( 'error adding item' );
         console.log( err );
     })
 } // end addItem
 
+function getItems(){
+    // GET call to server
+    $.ajax({
+        type: 'GET',
+        url: '/items'
+    }).then( function( response ){
+        console.log( 'back from GET:', response );
+        // loop through results
+        // display each on the DOM
+    }).catch( function( err ){
+        alert( 'error getting items' );
+        console.log( err );
+    })
+} // getItems
+
 function onReady(){
     $( '#addItemButton' ).on( 'click', addItem );
+    getItems();
 } // end onReady
